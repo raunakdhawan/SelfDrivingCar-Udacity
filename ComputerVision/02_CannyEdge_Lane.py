edae5516493c7af8444700959a10d7c0418de66e
RD <raunak.dhawan@gmail.com>
2019-07-12 22:36:44 +0200

    Add Hough Space for line detection

--- ComputerVision/02_CannyEdge_Lane.py
+++ ComputerVision/02_CannyEdge_Lane.py
@@ -1,28 +1,56 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import numpy as np
 import cv2
 
-# Load the image and show
+
+# Read in and grayscale the image
 image = mpimg.imread('exit-ramp.jpg')
-# plt.imshow(image)
+gray = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
 
-# Preprocess the image
-gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+# Define a kernel size and apply Gaussian smoothing
+kernel_size = 5
+blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
 
-# Define a kernel size for Gaussian smoothing / blurring
-# Note: this step is optional as cv2.Canny() applies a 5x5 Gaussian internally
-kernel_size = 7
-blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size), 0)
-
-# Define parameters for Canny and run it
-# NOTE: if you try running this code you might want to change these!
+# Define our parameters for Canny and apply
 low_threshold = 50
-high_threshold = 100
+high_threshold = 150
 edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
 
-# Display the image
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
-ax1.imshow(gray, cmap='Greys_r')
-ax2.imshow(blur_gray, cmap='Greys_r')
-ax3.imshow(edges, cmap='Greys_r')
+# Next we'll create a masked edges image using cv2.fillPoly()
+mask = np.zeros_like(edges)   
+ignore_mask_color = 255   
+
+# This time we are defining a four sided polygon to mask
+imshape = image.shape
+vertices = np.array([[(0,imshape[0]),(490, 290), (imshape[1],imshape[0])]], dtype=np.int32)
+cv2.fillPoly(mask, vertices, ignore_mask_color)
+masked_edges = cv2.bitwise_and(edges, mask)
+
+# Define the Hough transform parameters
+# Make a blank the same size as our image to draw on
+rho = 1 # distance resolution in pixels of the Hough grid
+theta = 1*np.pi/180 # angular resolution in radians of the Hough grid
+threshold = 15    # minimum number of votes (intersections in Hough grid cell)
+min_line_length = 40 #minimum number of pixels making up a line
+max_line_gap = 20    # maximum gap in pixels between connectable line segments
+line_image = np.copy(image)*0 # creating a blank to draw lines on
+
+# Run Hough on edge detected image
+# Output "lines" is an array containing endpoints of detected line segments
+lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]),
+                            min_line_length, max_line_gap)
+
+# Iterate over the output "lines" and draw lines on a blank image
+for line in lines:
+    for x1,y1,x2,y2 in line:
+        cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
+
+# Create a "color" binary image to combine with line image
+color_edges = np.dstack((edges, edges, edges)) 
+
+# Draw the lines on the edge image
+lines_edges = cv2.addWeighted(color_edges, 0.8, line_image, 1, 0) 
+plt.imshow(lines_edges)
 plt.show()
+
