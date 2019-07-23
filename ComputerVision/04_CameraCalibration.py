d2edd8e6e8b68f3173e64a078a54a0924a778cbe
RD <raunak.dhawan@gmail.com>
2019-07-23 18:20:54 +0200

    Add definition of objpoints and img points

--- ComputerVision/04_CameraCalibration.py
+++ ComputerVision/04_CameraCalibration.py
@@ -3,6 +3,11 @@ import cv2
 import glob
 import matplotlib.pyplot as plt
 
+'''
+Object Points : The ideal 3D points on the chess board 
+Image Points : The 2D points on the image of the chess board (determined by open cv)
+'''
+
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((6*8,3), np.float32)
 objp[:,:2] = np.mgrid[0:8, 0:6].T.reshape(-1,2)
